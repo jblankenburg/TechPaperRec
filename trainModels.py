4442cf20d99ee597f8573ea6149891661eff4311
Janelle <jjblankenburg@gmail.com>
2017-12-14 15:43:13 -0800

    Cleaned up the training file. Now to fix up the testing file accordingly. Then add the networks stuff.

--- trainModels.py
+++ trainModels.py
@@ -5,7 +5,7 @@ import gensim
 from gensim.models import Doc2Vec
 import gensim.models.doc2vec
 from gensim.models.doc2vec import TaggedDocument
-# from gensim.test.test_doc2vec import ConcatenatedDoc2Vec
+from gensim.test.test_doc2vec import ConcatenatedDoc2Vec
 from collections import OrderedDict
 import multiprocessing
 import cPickle as pickle
@@ -50,7 +50,6 @@ def trainCatModels(docs):
     cores = multiprocessing.cpu_count()
     assert gensim.models.doc2vec.FAST_VERSION > -1, "This will be painfully slow otherwise"
     # print gensim.models.word2vec.FAST_VERSION 
-    VOC_SIZE = 10000
 
     # train models - one per category
     for key in docs:
@@ -74,90 +73,80 @@ def trainCatModels(docs):
                 # # PV-DM w/ average
                 # Doc2Vec(dm=1, dm_mean=1, size=5, window=10, hs=0, min_count=1, iter=10, workers=cores),
 
-                # Doc2Vec(dm=1, dm_concat=1, size=5,  negative=5, hs=0, min_count=1, window=5, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
-                # Doc2Vec(dm=0, size=5,  negative=5, hs=0, min_count=1, window=5, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
-                # Doc2Vec(dm=1, dm_mean=1, size=5,  negative=5, hs=0, min_count=1, window=5, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
-
-                Doc2Vec(dm=1, dm_concat=1, size=5,  negative=0, hs=0, min_count=2, window=5, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
-                Doc2Vec(dm=0, size=5,  negative=0, hs=0, min_count=2, window=5, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
-                Doc2Vec(dm=1, dm_mean=1, size=5,  negative=0, hs=0, min_count=2, window=5, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
+                Doc2Vec(dm=1, dm_concat=1, size=5,  negative=5, hs=0, min_count=1, window=5, iter=10, workers=cores, ),
+                Doc2Vec(dm=0, size=5,  negative=5, hs=0, min_count=1, window=5, iter=10, workers=cores, ),
+                Doc2Vec(dm=1, dm_mean=1, size=5,  negative=5, hs=0, min_count=1, window=5, iter=10, workers=cores, ),
                 ]
-        # else:
-        #     # define models
-        #     simple_models = [
-        #         # PV-DM w/ concatenation - window=5 (both sides) approximates paper's 10-word total window size
-        #         Doc2Vec(dm=1, dm_concat=1, size=100, window=10, hs=0, min_count=2, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
-        #         # PV-DBOW 
-        #         Doc2Vec(dm=0, size=100, hs=0, min_count=2, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
-        #         # PV-DM w/ average
-        #         Doc2Vec(dm=1, dm_mean=1, size=100, window=10, negative=5, hs=0, min_count=2, iter=10, workers=cores, max_vocab_size=VOC_SIZE),
-        #         ]
-
-            # for item in docs[key]:
-            #     print item.tags
-
-
-            print simple_models[0].estimate_memory(vocab_size=VOC_SIZE)
-            print simple_models[0].scan_vocab(docs[key])
-            print len(simple_models[0].wv.vocab)
-            print simple_models[0].scale_vocab(min_count = 1, dry_run=True)
-            print len(simple_models[0].wv.vocab)
-            # print simple_models[0].finalize_vocab()            
-            # print len(simple_models[0].wv.vocab)
-
-        # # Speed up setup by sharing results of the 1st model's vocabulary scan
-        # startTimeVocab = datetime.now()
-        # print 'starting to build vocab: {}'.format(startTimeVocab)
-        # simple_models[0].build_vocab(doc)  # PV-DM w/ concat requires one special NULL word so it serves as template
-  
-        # # simple_models[0] = Doc2Vec.load('doc2vec_title_VOCAB.model')
-
-        # print 'vocab was built in {}\n'.format(datetime.now() - startTimeVocab)
-        # print len(simple_models[0].wv.vocab)
-
-        # startTimeVocab = datetime.now()
-        # print 'starting to save vocab model: {}'.format(startTimeVocab)
-        # simple_models[0].save("doc2vec_{}_VOCAB_8.model".format(key))
-        # print 'saved vocab model in {}\n'.format(datetime.now() - startTimeVocab)
-
-        # print(simple_models[0])
-        # for model in simple_models[1:]:
-        #     model.reset_from(simple_models[0])
-        #     print(model)
-        # models_by_name = OrderedDict((str(model), model) for model in simple_models)
-
-        # # NOT WORKING, IMPORT ERROR, try later if other models not sufficient
-        # # models_by_name['dbow+dmm'] = ConcatenatedDoc2Vec([simple_models[1], simple_models[2]])
-        # # models_by_name['dbow+dmc'] = ConcatenatedDoc2Vec([simple_models[1], simple_models[0]])
-
-        # # print 'starting to build vocab'
-        # # startTimeVocab = datetime.now()
-        # # model.build_vocab(doc)
-        # # print 'vocab was built in {}\n'.format(datetime.now() - startTimeVocab)
-
-        # # print model['domains']
-
-        # for name, train_model in models_by_name.items():
-        #     print 'starting to train model: {}'.format(name)
-        #     startTimeModel = datetime.now()
-        #     model.train(doc, total_examples=model.corpus_count, epochs=model.iter)
-        #     print 'model was trained in {}\n'.format(datetime.now() - startTimeModel)
-
-        #     model.save("doc2vec_{}_{}.model".format(key,name))
-        #     print "model was saved to: doc2vec_{}_{}.model".format(key,name)
-
-
-        # # print 'starting to train model'
-        # # startTimeModel = datetime.now()
-        # # model.train(doc, total_examples=model.corpus_count, epochs=model.iter)
-        # # print 'model was trained in {}\n'.format(datetime.now() - startTimeModel)
-
-
-        # # model.save("doc2vec_{}.model".format(key))
-        # # print "model was saved to: doc2vec_{}.model".format(key)
-
         else:
-            print 'skip {}'.format(key)
+            # define models
+            simple_models = [
+                # PV-DM w/ concatenation - window=5 (both sides) approximates paper's 10-word total window size
+                Doc2Vec(dm=1, dm_concat=1, size=100, window=10, hs=0, min_count=2, iter=10, workers=cores, ),
+                # PV-DBOW 
+                Doc2Vec(dm=0, size=100, hs=0, min_count=2, iter=10, workers=cores, ),
+                # PV-DM w/ average
+                Doc2Vec(dm=1, dm_mean=1, size=100, window=10, negative=5, hs=0, min_count=2, iter=10, workers=cores, ),
+                ]
+
+        # simple_models[0] = Doc2Vec.load('doc2vec_title_VOCAB.model')
+
+
+        # Speed up setup by sharing results of the 1st model's vocabulary scan
+        startTimeVocab = datetime.now()
+        print 'starting to build vocab: {}'.format(startTimeVocab)
+        simple_models[0].build_vocab(docs[key])  # PV-DM w/ concat requires one special NULL word so it serves as template  
+        print 'vocab was built in {}\n'.format(datetime.now() - startTimeVocab)
+        print 'len of vocab: {}'.format(len(simple_models[0].wv.vocab))
+
+        startTimeVocab = datetime.now()
+        print 'starting to save vocab model: {}'.format(startTimeVocab)
+        simple_models[0].save("models/doc2vec_{}_VOCAB.model".format(key))
+        print 'saved vocab model in {}\n'.format(datetime.now() - startTimeVocab)
+
+
+        startTimeVocab = datetime.now()
+        print 'starting to transfer vocab to other models: {}'.format(startTimeVocab)
+        print(simple_models[0])
+        for model in simple_models[1:]:
+            model.reset_from(simple_models[0])
+            print(model)
+        models_by_name = OrderedDict((str(model), model) for model in simple_models)
+        print 'transfered vocab in {}\n'.format(datetime.now() - startTimeVocab)
+
+        # NOT WORKING, IMPORT ERROR, try later if other models not sufficient
+        models_by_name['dbow+dmm'] = ConcatenatedDoc2Vec([simple_models[1], simple_models[2]])
+        models_by_name['dbow+dmc'] = ConcatenatedDoc2Vec([simple_models[1], simple_models[0]])
+
+        # print 'starting to build vocab'
+        # startTimeVocab = datetime.now()
+        # model.build_vocab(doc)
+        # print 'vocab was built in {}\n'.format(datetime.now() - startTimeVocab)
+
+        # print model['domains']
+
+        modelCount = 0
+        for name, train_model in models_by_name.items():
+
+            print 'starting to train model: {}'.format(name)
+            startTimeModel = datetime.now()
+            model.train(docs[key], total_examples=model.corpus_count, epochs=model.iter)
+            print 'model was trained in {}\n'.format(datetime.now() - startTimeModel)
+
+            model.save("models/doc2vec_{}_{}.model".format(key, modelCount))
+            print "model was saved to: models/doc2vec_{}_{}.model".format(key,modelCount)
+
+            modelCount +=1
+
+
+        # print 'starting to train model'
+        # startTimeModel = datetime.now()
+        # model.train(doc, total_examples=model.corpus_count, epochs=model.iter)
+        # print 'model was trained in {}\n'.format(datetime.now() - startTimeModel)
+
+
+        # model.save("doc2vec_{}.model".format(key))
+        # print "model was saved to: doc2vec_{}.model".format(key)
+
 
 # ------------------------------------------------------
 
